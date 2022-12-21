71ebd3f8bea5950d2bc3b2e78e89dddb457bacaf
Sergio Victor Nowosad Petruk <sergiovnp@gmail.com>
2022-12-20 23:53:47 -0300

    Views, Listar personas, templates y Staticfiles

--- personas/views.py
+++ personas/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Persona
 
-# Create your views here.
+
+class PersonaList(ListView):
+    model = Persona
+
+
