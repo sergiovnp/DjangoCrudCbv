499c131794c42e9e796d0f77cd891a16f443968e
Sergio Victor Nowosad Petruk <sergiovnp@gmail.com>
2022-12-21 23:17:03 -0300

    Views, Detalle de Persona

--- personas/urls.py
+++ personas/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PersonaList
+from .views import PersonaList, PersonaDetail
 
 urlpatterns = [
     path('', PersonaList.as_view(), name="persona_list"),
+    path('persona_detail/<int:pk>/', PersonaDetail.as_view(), name="persona_detail"),
 ]
--- personas/views.py
+++ personas/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Persona
 
 
 class PersonaList(ListView):
     model = Persona
 
+
+class PersonaDetail(DetailView):
+    model = Persona
 
