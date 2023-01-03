c595ab4f6ea6839808d8517930cb6dd929e37534
Sergio Victor Nowosad Petruk <sergiovnp@gmail.com>
2023-01-02 22:02:09 -0300

    Vista editar y eliminar

--- personas/urls.py
+++ personas/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PersonaList, PersonaDetail, PersonaCreate
+from .views import PersonaList, PersonaDetail, PersonaCreate, PersonaUpdate, PersonaDelete
 
 urlpatterns = [
     path('', PersonaList.as_view(), name="persona_list"),
     path('persona_detail/<int:pk>/', PersonaDetail.as_view(), name="persona_detail"),
     path('persona_create', PersonaCreate.as_view(), name="persona_create"),
+    path('persona_update/<int:pk>/', PersonaUpdate.as_view(), name="persona_update"),
+    path('persona_delete/<int:pk>/', PersonaDelete.as_view(), name="persona_delete"),
 ]
--- personas/views.py
+++ personas/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView,DeleteView
 from django.urls import reverse_lazy
 from .models import Persona
 
@@ -26,5 +26,15 @@ class PersonaCreate(CreateView):
     def get_success_url(self):
         return reverse_lazy('persona_list')
 
+class PersonaUpdate(UpdateView):
+    model = Persona
+    fields = ['nombre', 'apellido']
+    template_name_suffix = '_update_form'
+    def get_success_url(self):
+        return reverse_lazy('persona_list')
 
+class PersonaDelete(DeleteView):
+    model = Persona
+    def get_success_url(self):
+        return reverse_lazy('persona_list')
     
